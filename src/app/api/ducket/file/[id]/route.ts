f854cc2caaf6228a0f53650faecbd61f068f2ed8
oscarprdev <vlcoscar8@gmail.com>
2025-02-05 08:04:19 +0100

    Add get file endpoint

--- src/app/api/ducket/file/[id]/route.ts
+++ src/app/api/ducket/file/[id]/route.ts
@@ -1,6 +1,49 @@
-export function GET(request: Request) {
-  console.log(request);
-  return new Response('Hello World GET!');
+import { QUERIES } from '~/server/db/queries';
+
+async function validateBearerAuth(request: Request): Promise<string | undefined> {
+  const bearer = request.headers.get('Authorization');
+  return bearer?.split(' ')[1];
+}
+
+export async function GET(request: Request, context: { params: Promise<{ id: string }> }) {
+  try {
+    /**
+     * Validate bearer auth
+     */
+    const apiKey = await validateBearerAuth(request);
+    if (!apiKey) {
+      return new Response('Invalid bearer auth', { status: 401 });
+    }
+
+    /**
+     * Get file by param id
+     */
+    const { id } = await context.params;
+    const [file] = await QUERIES.getFileByName({ name: id });
+    if (!file) {
+      return new Response('File not found', { status: 404 });
+    }
+
+    /**
+     * Validate if project does exist based on projectId from file
+     */
+    const [project] = await QUERIES.getProject({ projectId: file?.projectId });
+    if (!project) {
+      return new Response('Project not found', { status: 404 });
+    }
+
+    /**
+     * Validate authorization by api key
+     */
+    if (project.api_key !== apiKey) {
+      return new Response('Api key not valid', { status: 401 });
+    }
+
+    return new Response(JSON.stringify({ fileUrl: file.fileUrl }), { status: 200 });
+  } catch (error) {
+    const errorMessage = error instanceof Error ? error.message : 'Internal Server Error';
+    return new Response(errorMessage, { status: 500 });
+  }
 }
 
 export function DELETE(request: Request) {
--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -1,40 +1,42 @@
 import { db } from '.';
-import type {
-  CreateFileInput,
-  CreateProjectInput,
-  GetProjectByTitleInput,
-  GetProjectInput,
-  GetProjectsInput,
-} from './queries.types';
-import { type Projects, files, projects } from './schema';
+import { type Files, type Projects, files, projects } from './schema';
 import { eq } from 'drizzle-orm';
 
 export const QUERIES = {
-  getProjects: function ({ ownerId }: GetProjectsInput): Promise<Projects[]> {
+  getProjects: function ({ ownerId }: { ownerId: string }): Promise<Projects[]> {
     return db.select().from(projects).where(eq(projects.ownerId, ownerId)).limit(10).offset(0);
   },
-  getProject: function ({ projectId }: GetProjectInput): Promise<Projects[]> {
+  getProject: function ({ projectId }: { projectId: string }): Promise<Projects[]> {
     return db.select().from(projects).where(eq(projects.id, projectId));
   },
-  getProjectByTitle: function ({ title }: GetProjectByTitleInput): Promise<Projects[]> {
+  getProjectByTitle: function ({ title }: { title: string }): Promise<Projects[]> {
     return db.select().from(projects).where(eq(projects.title, title));
+  },
+  getFileByName: function ({ name }: { name: string }): Promise<Files[]> {
+    return db.select().from(files).where(eq(files.fileName, name));
   },
 };
 
 export const MUTATIONS = {
-  createProject: function ({ ownerId, title, apiKey }: CreateProjectInput): Promise<Projects[]> {
+  createProject: function (input: {
+    ownerId: string;
+    title: string;
+    apiKey: string;
+  }): Promise<Projects[]> {
+    const { ownerId, title, apiKey } = input;
     return db.insert(projects).values({
       ownerId,
       title,
       api_key: apiKey,
     });
   },
-  createFile: function ({
-    projectId,
-    fileName,
-    type,
-    fileUrl,
-  }: CreateFileInput): Promise<Projects[]> {
+  createFile: function (input: {
+    projectId: string;
+    fileName: string;
+    type: string;
+    fileUrl: string;
+  }): Promise<Projects[]> {
+    const { projectId, fileName, type, fileUrl } = input;
     return db.insert(files).values({
       projectId,
       fileName,
