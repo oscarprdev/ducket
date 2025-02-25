9cab30fadcbef0786b31b97d978a5f1c70ba6bf2
oscarprdev <vlcoscar8@gmail.com>
2025-02-25 15:40:40 +0100

    refactor: Improve developer documentation code block styling

--- src/components/website/developers/docs-get.tsx
+++ src/components/website/developers/docs-get.tsx
@@ -7,7 +7,42 @@ export function DocsGet() {
       <p className="mt-4 max-w-[80ch] text-muted-foreground">
         The <code>getFile</code> method returns a file URL that you can use in your application.
       </p>
-      <div className="mt-6 rounded-md bg-zinc-950 p-4">
+      <div className="mt-6 rounded-md bg-zinc-950 px-4">
+        <div className="border-b border-zinc-800">
+          <div className="flex w-full items-start py-2">
+            <span className="size-2.5 rounded-full border bg-destructive"></span>
+            <span className="size-2.5 rounded-full border bg-tertiary"></span>
+            <span className="size-2.5 rounded-full border bg-accent"></span>
+          </div>
+        </div>
+        <div className="space-y-4 py-4">
+          <code className="block font-mono text-xs">
+            <span className="text-zinc-500">import</span>
+            <span className="text-white"> {'{ '}</span>
+            <span className="text-white">Bucket</span>
+            <span className="text-white">{' }'}</span>
+            <span className="text-zinc-500"> from </span>
+            <span className="text-accent">{`'@bucket'`}</span>
+            <span className="text-white">;</span>
+          </code>
+          <code className="block font-mono text-xs">
+            <span className="text-zinc-500">const</span>
+            <span className="text-white"> bucket = </span>
+            <span className="text-zinc-500">new</span>
+            <span className="text-white"> Bucket({'{'}</span>
+            <div className="pl-4">
+              <span className="text-white">useDucket:</span>
+              <span className="text-accent"> true</span>
+              <span className="text-white">,</span>
+            </div>
+            <div className="pl-4">
+              <span className="text-white">apiKey: </span>
+              <span className="text-accent">gI6ZDBq1GV....fv2t96Xd</span>
+            </div>
+            <span className="text-white">{'})'}</span>
+            <span className="text-white">;</span>
+          </code>
+        </div>
         <code className="block font-mono text-xs">
           <span className="text-zinc-500">export const</span>
           <span className="text-white"> getFile = </span>
--- src/components/website/developers/docs-upload.tsx
+++ src/components/website/developers/docs-upload.tsx
@@ -10,7 +10,42 @@ export function DocsUpload() {
         success response when the file is successfully uploaded.
       </p>
 
-      <div className="mt-6 rounded-md bg-zinc-950 p-4">
+      <div className="mt-6 rounded-md bg-zinc-950 px-4">
+        <div className="border-b border-zinc-800">
+          <div className="flex w-full items-start py-2">
+            <span className="size-2.5 rounded-full border bg-destructive"></span>
+            <span className="size-2.5 rounded-full border bg-tertiary"></span>
+            <span className="size-2.5 rounded-full border bg-accent"></span>
+          </div>
+        </div>
+        <div className="space-y-4 py-4">
+          <code className="block font-mono text-xs">
+            <span className="text-zinc-500">import</span>
+            <span className="text-white"> {'{ '}</span>
+            <span className="text-white">Bucket</span>
+            <span className="text-white">{' }'}</span>
+            <span className="text-zinc-500"> from </span>
+            <span className="text-accent">{`'@bucket'`}</span>
+            <span className="text-white">;</span>
+          </code>
+          <code className="block font-mono text-xs">
+            <span className="text-zinc-500">const</span>
+            <span className="text-white"> bucket = </span>
+            <span className="text-zinc-500">new</span>
+            <span className="text-white"> Bucket({'{'}</span>
+            <div className="pl-4">
+              <span className="text-white">useDucket:</span>
+              <span className="text-accent"> true</span>
+              <span className="text-white">,</span>
+            </div>
+            <div className="pl-4">
+              <span className="text-white">apiKey: </span>
+              <span className="text-accent">gI6ZDBq1GV....fv2t96Xd</span>
+            </div>
+            <span className="text-white">{'})'}</span>
+            <span className="text-white">;</span>
+          </code>
+        </div>
         <code className="block font-mono text-xs">
           <span className="text-zinc-500">export const</span>
           <span className="text-white"> uploadFile = </span>
